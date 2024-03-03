447441e17f4192eebcbca9ef5a91c280de48bc08
Abhishek Pandey <pandeyabhishek0922@gmail.com>
2024-03-03 11:24:30 +0530

    Upload to GitHub

---
+++ src/models/expense.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const expenseSchema = new Schema(
+    {
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+        client: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+        dueDate: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+        },
+        items: [
+            {
+                name: String,
+                qty: Number,
+                price: Number,
+            },
+        ],
+        totalAmount: {
+            type: Number,
+            default: 0,
+            required: true,
+        },
+        status: {
+            type: Boolean,
+            default: false,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Expense = mongoose.model("Expense", expenseSchema);
